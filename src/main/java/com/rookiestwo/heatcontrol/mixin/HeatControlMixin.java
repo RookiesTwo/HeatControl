5b9b4973df63c79c300f9862009e31b6aac877c5
肉鸡二号 <rookies_two@yeah.net>
2023-06-12 00:02:03 +0800

    文件重构
    将示例mod文件结构重命名为HeatControl
    添加了MOD的图标

--- src/main/java/com/rookiestwo/heatcontrol/mixin/HeatControlMixin.java
+++ src/main/java/com/rookiestwo/heatcontrol/mixin/HeatControlMixin.java
@@ -1,6 +1,7 @@
-package net.fabricmc.example.mixin;
+package com.rookiestwo.heatcontrol.mixin;
 
-import net.fabricmc.example.ExampleMod;
+
+import com.rookiestwo.heatcontrol.HeatControl;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +9,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class HeatControlMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		HeatControl.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
