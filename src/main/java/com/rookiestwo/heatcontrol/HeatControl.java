ac563f813f23ef7de3c9b39c8279872cae0242bc
CreeperTitan <2534459614@qq.com>
2023-06-12 00:21:56 +0800

    未完成的温度计物品

--- src/main/java/com/rookiestwo/heatcontrol/HeatControl.java
+++ src/main/java/com/rookiestwo/heatcontrol/HeatControl.java
@@ -1,21 +1,20 @@
 package com.rookiestwo.heatcontrol;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class HeatControl implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Heat_Control");
 
+	public static final Item thermometer=new Item(new FabricItemSettings());
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	public void onInitialize(){
+		Registry.register(Registry.ITEM,new Identifier("HeatControl","thermometer"),thermometer);
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
