844ed19079ce08ad78b7dba042e677bb893078ef
CreeperTitan <2534459614@qq.com>
2023-06-12 00:31:43 +0800

    添加了温度计物品及纹理贴图

--- src/main/java/com/rookiestwo/heatcontrol/HeatControl.java
+++ src/main/java/com/rookiestwo/heatcontrol/HeatControl.java
@@ -5,12 +5,16 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class HeatControl implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 
+	public static final Logger LOGGER= LoggerFactory.getLogger("Heat_Control");
 	public static final Item thermometer=new Item(new FabricItemSettings());
 	@Override
 	public void onInitialize(){
