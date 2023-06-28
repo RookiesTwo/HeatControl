ac6e15ee894cd109495e46c5c88f013101784696
CreeperTitan <2534459614@qq.com>
2023-06-28 21:51:08 +0800

    添加了合成表，修改了炽热道具的命名

--- src/main/java/com/rookiestwo/heatcontrol/items/BlazingNecklace.java
+++ src/main/java/com/rookiestwo/heatcontrol/items/BlazingNecklace.java
@@ -2,8 +2,8 @@ package com.rookiestwo.heatcontrol.items;
 
 import net.minecraft.item.Item;
 
-public class BlazingCirclet extends Item {
-    public BlazingCirclet(Settings settings){
+public class BlazingNecklace extends Item {
+    public BlazingNecklace(Settings settings){
         super(settings);
     }
 }
