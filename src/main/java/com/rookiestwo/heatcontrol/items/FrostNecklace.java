0923d70162d290c14ec94c340d60543fb571ba18
CreeperTitan <2534459614@qq.com>
2023-07-13 11:16:30 +0800

    添加了饰品（暂时仅炽热项链

--- src/main/java/com/rookiestwo/heatcontrol/items/FrostNecklace.java
+++ src/main/java/com/rookiestwo/heatcontrol/items/FrostNecklace.java
@@ -1,9 +1,27 @@
 package com.rookiestwo.heatcontrol.items;
 
-import net.minecraft.item.Item;
+import com.google.common.collect.Multimap;
+import com.rookiestwo.heatcontrol.tools.HeatAttributeManager;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketItem;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.item.ItemStack;
 
-public class FrostNecklace extends Item {
+import java.util.UUID;
+
+public class FrostNecklace extends TrinketItem {
     public FrostNecklace(Settings settings){
     super(settings);
+    }
+    public Multimap<EntityAttribute, EntityAttributeModifier> getModifier(
+            ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid){
+        var modifiers=super.getModifiers(stack,slot,entity,uuid);
+        modifiers.put(
+                HeatAttributeManager.MAX_TEMPERATURE, new EntityAttributeModifier(uuid,"heatcontrol:max_temperature",20.0,EntityAttributeModifier.Operation.ADDITION));
+        modifiers.put(
+                HeatAttributeManager.MIN_TEMPERATURE, new EntityAttributeModifier(uuid,"heatcontrol:min_temperature",10.0,EntityAttributeModifier.Operation.ADDITION));
+        return modifiers;
     }
 }
