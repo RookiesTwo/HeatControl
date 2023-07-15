ac2e4030b8698962eae27754e6e810e6d14e58ba
肉鸡二号 <rookies_two@yeah.net>
2023-07-15 19:56:52 +0800

    方块光照实现框架

--- src/main/java/com/rookiestwo/heatcontrol/HeatControl.java
+++ src/main/java/com/rookiestwo/heatcontrol/HeatControl.java
@@ -6,15 +6,14 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import net.minecraft.server.network.ServerPlayerEntity;
 
 public class HeatControl implements ModInitializer {
 
@@ -22,24 +21,22 @@ public class HeatControl implements ModInitializer {
 	public static final Logger LOGGER= LoggerFactory.getLogger("Heat_Control");
 
 
-
 	//创建并实例化模组物品组和物品
-	public static final ItemGroup HC_ITEM_GROUP= FabricItemGroupBuilder.build(
-			new Identifier("heatcontrol","hc_item_group"),
-			()->new ItemStack(HCItemRegistry.heat_control_icon)
+	public static final ItemGroup HC_ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier(MOD_ID, "hc_item_group"),
+			() -> new ItemStack(HCItemRegistry.heat_control_icon)
 	);
 
 	//注册EntityAttribute
-	public static final EntityAttribute env_temperature=Registry.register(Registry.ATTRIBUTE,new Identifier("heatcontrol","env_temperature"),HeatAttributeManager.ENV_ATTRIBUTE);
-	public static final EntityAttribute min_temperature=Registry.register(Registry.ATTRIBUTE,new Identifier("heatcontrol","min_temperature"),HeatAttributeManager.MIN_TEMPERATURE);
-	public static final EntityAttribute max_temperature=Registry.register(Registry.ATTRIBUTE,new Identifier("heatcontrol","max_temperature"),HeatAttributeManager.MAX_TEMPERATURE);
-
+	public static final EntityAttribute env_temperature = Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "env_temperature"), HeatAttributeManager.ENV_ATTRIBUTE);
+	public static final EntityAttribute min_temperature = Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "min_temperature"), HeatAttributeManager.MIN_TEMPERATURE);
+	public static final EntityAttribute max_temperature = Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "max_temperature"), HeatAttributeManager.MAX_TEMPERATURE);
+	private final int ticksPerTask = 10;
 	//tick的计数器以及几个tick执行一次
-	private int tickCounter=0;
-	private final int ticksPerTask=10;
+	private int tickCounter = 0;
 
 	@Override
-	public void onInitialize(){
+	public void onInitialize() {
 		//注册模组物品
 		HCItemRegistry.registryItem();
 
@@ -55,6 +52,8 @@ public class HeatControl implements ModInitializer {
 				//遍历玩家并刷新玩家的环境温度值
 				for (ServerPlayerEntity player : players) {
 					player.getAttributeInstance(HeatControl.env_temperature).setBaseValue(HeatAttributeManager.calculateTemperatureValue(player));
+
+					HeatAttributeManager.applyBlockLightEffect(player);
 				}
 				//LOGGER.debug("Player Temperature set!");
 			}
