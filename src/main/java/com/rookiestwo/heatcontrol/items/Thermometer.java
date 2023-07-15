1b7ad1a725ed551aeccf332b3da40820f4ccf8b0
肉鸡二号 <rookies_two@yeah.net>
2023-07-15 16:51:27 +0800

    Update Thermometer.java

--- src/main/java/com/rookiestwo/heatcontrol/items/Thermometer.java
+++ src/main/java/com/rookiestwo/heatcontrol/items/Thermometer.java
@@ -16,10 +16,6 @@ public class Thermometer extends Item {
         super(settings);
     }
 
-    private final static int decimalPlaces = 2;
-    // 要保留的小数位数
-    //private final static DecimalFormat outputFormat = new DecimalFormat("#." + "0".repeat(decimalPlaces));
-    //保留小数格式化器
     @Override
     //手持温度计右键：在客户端的聊天栏显示温度数值
     public TypedActionResult<ItemStack>use(World world, PlayerEntity playerEntity, Hand hand){
