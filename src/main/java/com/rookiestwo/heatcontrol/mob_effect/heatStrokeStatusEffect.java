4cc51353476dcb900b39290d9357a7f088b4b71f
肉鸡二号 <rookies_two@yeah.net>
2023-07-21 18:21:24 +0800

    实现了各种效果

--- src/main/java/com/rookiestwo/heatcontrol/mob_effect/heatStrokeStatusEffect.java
+++ src/main/java/com/rookiestwo/heatcontrol/mob_effect/heatStrokeStatusEffect.java
@@ -1,9 +1,16 @@
 package com.rookiestwo.heatcontrol.mob_effect;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+
+import java.util.UUID;
 
 public class heatStrokeStatusEffect extends StatusEffect {
     public heatStrokeStatusEffect() {
@@ -17,14 +24,23 @@ public class heatStrokeStatusEffect extends StatusEffect {
 
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-        if (entity instanceof PlayerEntity) {
-            if (((PlayerEntity) entity).getMainHandStack().getItem().isDamageable()) {
-                //((PlayerEntity)entity).dropItem(entity.getMainHandStack(),false,true);
-                if(!entity.world.isClient()){
-                    ((PlayerEntity) entity).dropItem(entity.getMainHandStack(), false, true);
-                    ((PlayerEntity) entity).getInventory().dropSelectedItem(true);
+        if (entity instanceof PlayerEntity player) {
+            if (player.getMainHandStack().getItem().isDamageable()) {
+                if (!entity.world.isClient()) {
+                    player.dropItem(entity.getMainHandStack(), false, false);
+                    player.getInventory().setStack(player.getInventory().selectedSlot, ItemStack.EMPTY);
+
+                    player.setOnFireFor(3);
                 }
             }
         }
+    }
+
+    @Override
+    public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, UUID.randomUUID().toString(), -0.2, EntityAttributeModifier.Operation.MULTIPLY_BASE);
+        addAttributeModifier(EntityAttributes.GENERIC_MAX_HEALTH, UUID.randomUUID().toString(), -4.0, EntityAttributeModifier.Operation.ADDITION);
+        entity.damage(DamageSource.ON_FIRE, 4);
+        super.onApplied(entity, attributes, amplifier);
     }
 }
